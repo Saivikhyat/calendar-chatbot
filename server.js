41f36c0a499d1fd0cb6b325f7c8751aa8b0b276a
vikhy <admin@MacBookPro.ht.home>
2026-08-30 15:33:59 -0400

    Fix Express 5 wildcard route

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
